7d40986ba74a64dab522c86e97acb785fd8a7d16
bhoju jamre <bhojeshwarjamre13@gmail.com>
2024-05-10 22:29:43 +0530

    commit

---
+++ 01_basic/operations.js
@@ -0,0 +1,33 @@
+
+let val = 3
+
+console.log(val**val)//pow operator
+
+let str1="Hello"
+let str2="bhoju"
+
+console.log(str1+str2)
+
+console.log(1+"2") //12
+
+console.log("2"+1) //21
+
+console.log(1+2+"3") //33 not recommended
+
+console.log(+true); //1
+//console.log(true+); //error
+console.log(+""); //0
+
+comparisons
+
+console.log("2">1); //true
+
+console.log(null==0); //false
+
+console.log(null>=0); //null is converted to number then compared //true
+
+console.log(undefined > 0); //false
+
+//=== strict check
+
+console.log("2"===2); //false this is strict check
