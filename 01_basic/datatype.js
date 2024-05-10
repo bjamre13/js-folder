7ba7d468751a8d678bf25e16334ed39027f62412
bhoju jamre <bhojeshwarjamre13@gmail.com>
2024-05-10 22:06:54 +0530

    commit

---
+++ 01_basic/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as new version
+
+// alert("hello") using node js not browser
+
+let score=33
+
+console.log(typeof score) // typeof(score)
+
+let val="33ab"
+let val1=Number(val) // String() Boolean()
+console.log(val1)//NaN
+console.log(typeof val1) // number
+
+//true=1 false=0
+
+let isLogIn=1 //"Hello"--> true
+
+// isLongIn="" ,0 ,-->false
+
+console.log(typeof Boolean(isLogIn))
+
---
+++ 01_basic/variables.js
@@ -0,0 +1,8 @@
+
+const accId=1234;
+let accEmail="bjam13@gmal.com"
+var accPass="12345" //not to use bcz of block and funstional scope
+let accState;
+// accId=2
+console.log(accId)
+console.table([accEmail,accId,accPass,accState])
