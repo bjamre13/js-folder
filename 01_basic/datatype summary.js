9ebcba3678fea3fc857b40795d704c5767515a29
bhoju jamre <bhojeshwarjamre13@gmail.com>
2024-05-10 22:50:11 +0530

    commit

---
+++ 01_basic/datatype summary.js
@@ -0,0 +1,38 @@
+// // primitive these are call  by value
+
+// //String,
+// // Number(1,1.0),Boolean,null,undefined,Symbol,bigint
+
+// let val1; //value assigned will be undefined
+// const id=Symbol('123')
+// const anId=Symbol('123') //false
+// console.log(id==anId);
+
+// const bigInt=873476287463n;
+// console.log(typeof bigInt);
+
+// // reference or not primitive  
+
+// //Arays, Objects, function
+
+// const arr=["helo","bhoj","wru"];
+// console.log(arr);
+
+// //key : val pair objects
+// let myObj={
+//     id1:132,
+//     name:"bhoj"
+// }
+
+// const myfun = function(){
+//     console.log("hello how'z you");
+// }
+// console.log(myfun)//functionobject
+
+//typeof null==object
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof bigint);
+//type
