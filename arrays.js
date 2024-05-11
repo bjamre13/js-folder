bee53294b9aaf09971cdc006d26708dd06365ff5
bhoju jamre <bhojeshwarjamre13@gmail.com>
2024-05-11 15:50:43 +0530

    commit changes

--- arrays.js
+++ arrays.js
@@ -6,44 +6,44 @@
 const heros=["marvel","dc"]
 
 const arr1 =new Array(1,2,23,445);
-// console.log(arr1);
+console.log(arr1);
 
-//array methods
-// arr1.push(-45)
-// console.log(arr1);
+array methods
+arr1.push(-45)
+console.log(arr1);
 
-// arr1.pop() // removes last method
-// console.log(arr1);
+arr1.pop() // removes last method
+console.log(arr1);
 
-// arr1.unshift(9);
-// console.log(arr1); //insert at strting
+arr1.unshift(9);
+console.log(arr1); //insert at strting
 
-// // arr1.shift();
-// // console.log(arr1); //removes first elem
+// arr1.shift();
+// console.log(arr1); //removes first elem
 
-// console.log(arr1.includes(9)); //elem constins or not
-// console.log(arr1.indexOf(9));
+console.log(arr1.includes(9)); //elem constins or not
+console.log(arr1.indexOf(9));
 
-// let arr=arr1.join();//binds and converts into string
+let arr=arr1.join();//binds and converts into string
 
-// console.log(arr1);
+console.log(arr1);
 
-// console.log(typeof arr);
+console.log(typeof arr);
 
-//slice, splice
+slice, splice
 
-// console.log(arr1.slice(1,3));
-// console.log(arr1);
+console.log(arr1.slice(1,3));
+console.log(arr1);
 
-// console.log(arr1.splice(1,3)); // changes the original array itself
-// console.log(arr1);
+console.log(arr1.splice(1,3)); // changes the original array itself
+console.log(arr1);
 
 
 let mar_heros=["iron","thor","capAmr"]
 let dc_heros=["super","flash","batman"]
 
-// mar_heros.push(dc_heros)
-// console.log(mar_heros[3][1]);
+mar_heros.push(dc_heros)
+console.log(mar_heros[3][1]);
 
 let newAll=mar_heros.concat(dc_heros)
 console.log(newAll);
